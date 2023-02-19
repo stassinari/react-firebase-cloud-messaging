bd32557e11bce7f08fd2d706e3f3c1979ec20989
Saverio Tassinari <saverio.tassinari@gmail.com>
2023-02-19 01:12:11 +0000

    added some styles, re-did toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,127 @@
 import { MessagePayload, NotificationPayload } from "firebase/messaging";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { PropsWithChildren, useState } from "react";
+import { toast, ToastContainer, ToastContentProps } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { fetchToken, onMessageListener } from "./firebase";
 
-type Notification = NotificationPayload;
+type NotificationProps = {
+  notificationProps: NotificationPayload;
+} & Partial<ToastContentProps>;
 
-function App() {
-  const [show, setShow] = useState(false);
-  const [notification, setNotification] = useState<Notification>({
-    title: "",
-    body: "",
-  });
+const Notification = ({
+  notificationProps,
+  toastProps,
+  closeToast,
+}: NotificationProps) => (
+  <>
+    <span className="font-bold mb-2">{notificationProps.title}</span>
+    <p className="text-sm">{notificationProps.body}</p>
+  </>
+);
+export const App = () => {
   const [isTokenFound, setTokenFound] = useState(false);
 
   onMessageListener()
     .then((payload: MessagePayload) => {
-      setShow(true);
-      setNotification({
-        title: payload.notification?.title,
-        body: payload.notification?.body,
-      });
+      toast(
+        <Notification
+          notificationProps={{
+            title: payload.notification?.title,
+            body: payload.notification?.body,
+          }}
+        />
+      );
       console.log(payload);
     })
     .catch((err) => console.log("failed: ", err));
 
   return (
-    <>
-      {/* <Toast
-        onClose={() => setShow(false)}
-        show={show}
-        delay={3000}
-        autohide
-        animation
-        style={{
-          position: "absolute",
-          top: 20,
-          right: 20,
-          minWidth: 200,
-        }}
-      >
-        <Toast.Header>
-          <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-          <strong className="mr-auto">{notification.title}</strong>
-          <small>just now</small>
-        </Toast.Header>
-        <Toast.Body>{notification.body}</Toast.Body>
-      </Toast> */}
-      <div className="App">
-        <button onClick={() => fetchToken(setTokenFound)}>
+    <div className="mx-auto max-w-lg px-4 pt-12">
+      <h1 className="text-2xl font-bold mb-4">React Firebase Messaging PWA</h1>
+      <ToastContainer />
+
+      <div className="bg-blue-200 p-4 rounded-lg">
+        <Button onClick={() => fetchToken(setTokenFound)}>
           I want to receive notifications!
-        </button>
-        <div>
+        </Button>
+        <p>
           {isTokenFound
             ? "Notification permission enabled 👍🏻"
             : "Need notification permission ❗️"}
-        </div>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1 className="text-3xl font-bold underline">Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setShow(true)}>Show Toast</button>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
         </p>
       </div>
-    </>
+      <div className="my-4">
+        <p>
+          This is a small demo repo to get started with Firebase Cloud Messaging
+          within a React application bootstrapped with Vite + TypeScript.
+        </p>
+
+        <p>This is built with:</p>
+
+        <ul className="my-2">
+          <li>
+            <a href="https://reactjs.org/">React</a>
+          </li>
+          <li>
+            <a href="https://vitejs.dev/">Vite</a> +{" "}
+            <a href="https://www.typescriptlang.org/">TypeScript</a>
+          </li>
+          <li>
+            <a href="https://firebase.google.com/">Firebase</a>
+          </li>
+          <li>
+            <a href="https://tailwindcss.com/">Tailwind</a>
+          </li>
+          <li>
+            <a href="https://vite-pwa-org.netlify.app/">Vite PWA plugin</a>
+          </li>
+          <li>
+            <a href="https://fkhadra.github.io/react-toastify/introduction">
+              react-tostify
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ButtonSecondary
+          onClick={() =>
+            toast(
+              <Notification
+                notificationProps={{
+                  title: "Hello I'm a toast! 🍞",
+                  body: "Nothing much to say about it, is there?",
+                }}
+              />
+            )
+          }
+        >
+          Show an example toast
+        </ButtonSecondary>
+      </div>
+    </div>
   );
+};
+
+interface ButtonProps extends PropsWithChildren {
+  onClick: () => void;
 }
 
-export default App;
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => (
+  <button
+    type="button"
+    onClick={onClick}
+    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+  >
+    {children}
+  </button>
+);
+
+const ButtonSecondary: React.FC<ButtonProps> = ({ children, onClick }) => (
+  <button
+    type="button"
+    onClick={onClick}
+    className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+  >
+    {children}
+  </button>
+);
