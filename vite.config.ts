d3c65801f2ce33de708e5a12c5f2d89fbb9bbb4b
Saverio Tassinari <saverio.tassinari@gmail.com>
2023-02-25 16:36:03 +0000

    minimum requirements for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,26 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+
+      includeAssets: ["fire.svg", "assets/apple-icon-180.png"],
+      manifest: {
+        name: "react-firebase-cloud-messaging",
+        short_name: "rfcm",
+        description: "An example React PWA for Firebase Cloud Messaging",
+        theme_color: "#BFDBFE",
+        icons: [
+          {
+            src: "assets/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
 });
